672093ee51447fb9976528ca09e292e709c17c61
Anon <anon@noreply.com>
2019-08-03 23:20:38 -0500

    Adding routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+import { CharacterAddComponent } from './character-add/character-add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'characters',
+    component: CharactersComponent,
+    data: { title: 'List of Characters' }
+  },
+  {
+    path: 'character-details/:id',
+    component: CharacterDetailComponent,
+    data: { title: 'Character Details' }
+  },
+  {
+    path: 'character-add',
+    component: CharacterAddComponent,
+    data: { title: 'Add Character' }
+  },
+  {
+    path: '',
+    redirectTo: '/characters',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
