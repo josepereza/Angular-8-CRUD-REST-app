a1c43285ba02d4fb48bac70eef438ea7deeb0c09
Anon <anon@noreply.com>
2019-08-04 23:56:08 -0500

    Fetching character data to be viewed.

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
+import { Character } from '../character';
+
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
@@ -8,11 +10,25 @@ import { ApiService } from '../api.service';
 })
 export class CharactersComponent implements OnInit {
 
+  displayedColumns: string[] = ['character_name', 'character_description'];
+  data: Character[] = [];
+  isLoadingResults = true;
+
   constructor(
     private api: ApiService
   ) { }
 
   ngOnInit() {
+    this.api.getCharacters()
+      .subscribe(res => {
+        this.data = res;
+        console.log(this.data);
+        this.isLoadingResults = false;
+      },
+        err => {
+          console.log(err);
+          this.isLoadingResults = false;
+      });
   }
 
 }
