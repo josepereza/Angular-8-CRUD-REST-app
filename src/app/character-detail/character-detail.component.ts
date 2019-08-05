997599bc3e364bb087f4fafc48a772000bf37930
Anon <anon@noreply.com>
2019-08-05 18:55:27 -0500

    Preparing to show and delete Characters.

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Character } from '../character';
 
 @Component({
   selector: 'app-character-detail',
   templateUrl: './character-detail.component.html',
   styleUrls: ['./character-detail.component.css']
 })
+
 export class CharacterDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
