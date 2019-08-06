71a719d2f0e0db84371aaf3916f961d5373fb95b
Anon <anon@noreply.com>
2019-08-05 19:05:26 -0500

    Added getCharacterDetails and delete Character by id.

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -11,6 +11,9 @@ import { Character } from '../character';
 
 export class CharacterDetailComponent implements OnInit {
 
+  character: Character = { id: null, character_name: '', character_description: '', character_level: null, update_at: null };
+  isLoadingResults = true;
+
   constructor(
     private route: ActivatedRoute,
     private api: ApiService,
@@ -18,6 +21,28 @@ export class CharacterDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getCharacterDetails(this.route.snapshot.params['id']);
+  }
+
+  getCharacterDetails(id) {
+    this.api.getCharacter(id)
+      .subscribe(data => {
+        this.character = data;
+        console.log(this.character);
+        this.isLoadingResults = false;
+      });
+  }
+
+  deleteCharacter(id) {
+    this.isLoadingResults = true;
+    this.api.deleteCharacter(id)
+      .subscribe(res => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/characters']);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
