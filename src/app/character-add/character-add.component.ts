36a6e613729ab60a6ffbc08d7e3ea298095b49d3
Anon <anon@noreply.com>
2019-08-05 19:41:22 -0500

    Added POST request and fields for new character to be added.

--- src/app/character-add/character-add.component.ts
+++ src/app/character-add/character-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-character-add',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterAddComponent implements OnInit {
 
-  constructor() { }
+  characterForm: FormGroup;
+  character_name: string = '';
+  character_description: string = '';
+  character_level = null;
+  updated_at: Date = null;
+  isLoadingResults = false;
+
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.characterForm = this.formBuilder.group({
+      'character_name': [null, Validators.required],
+      'character_description': [null, Validators.required],
+      'character_level': [null, Validators.required],
+      'updated_at': [null, Validators.required]
+    });
+  }
+
+  onFormSubmit(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.addCharacter(form)
+      .subscribe(res => {
+        let id = res['id'];
+        this.isLoadingResults = false;
+        this.router.navigate(['/character-details', id]);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
