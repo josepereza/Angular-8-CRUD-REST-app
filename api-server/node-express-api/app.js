26a1f157570daf1085c305af7c6a231bf55ea261
Anon <anon@noreply.com>
2019-08-06 17:07:52 -0500

    Fixed bug caused by bodyParser.

--- api-server/node-express-api/app.js
+++ api-server/node-express-api/app.js
@@ -30,13 +30,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json);
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/characters', characters);
-// app.use('/character', characters);
+app.use('/character', characters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
